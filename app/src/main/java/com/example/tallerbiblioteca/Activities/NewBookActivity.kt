718a2e6a08524cf260c2065b415332d17b02fca8
aacm13 <00354817@uca.edu.sv>
2019-05-23 22:30:30 -0600

    'xml'

--- app/src/main/java/com/example/tallerbiblioteca/Activities/NewBookActivity.kt
+++ app/src/main/java/com/example/tallerbiblioteca/Activities/NewBookActivity.kt
@@ -8,6 +8,6 @@ class NewBookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_book)
+        setContentView(R.layout.content_main)
     }
 }
