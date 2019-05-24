66e72e5e5eb1aee3c260c7eb3193a6bb618512a5
aacm13 <00354817@uca.edu.sv>
2019-05-23 23:11:07 -0600

    detalle de libro xml

--- app/src/main/java/com/example/tallerbiblioteca/Activities/NewBookActivity.kt
+++ app/src/main/java/com/example/tallerbiblioteca/Activities/NewBookActivity.kt
@@ -8,6 +8,6 @@ class NewBookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.content_main)
+        setContentView(R.layout.book_list)
     }
 }
